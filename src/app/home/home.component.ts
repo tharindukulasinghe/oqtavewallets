a542be7158a07344573062a49e5dfb43fa2ad928
rdtharindudk <dilshantharinduk@gmail.com>
2018-12-30 18:55:36 +0530

    category fields implementation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+  catogeries = [
+    { name: "Men's Wallets", key: "mw" },
+    { name: "Women's Wallets", key: "ww" },
+    { name: "Men's Watches", key: "mwa" },
+    { name: "Women's Watches", key: "wwa" },
+    { name: "Accessories", key: "ac" }
+  ];
 
-  constructor() { }
+  category: String;
 
-  ngOnInit() {
+  constructor(route: ActivatedRoute) {
+    route.queryParamMap.subscribe(params => {
+      this.category = params.get("category");
+    });
   }
 
+  ngOnInit() {}
 }
